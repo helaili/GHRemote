c98329d02a5935bca09be6e2bb15256bf21f080d
Alain Hélaïli <xxxxhelaili@github.com>
2015-12-12 07:26:14 +0100

    fixing labels

--- modules/GitHubAPI/client/config/GitHubAPI.client.config.js
+++ modules/GitHubAPI/client/config/GitHubAPI.client.config.js
@@ -5,7 +5,7 @@ angular.module('gitHubAPI').run(['Menus',
   function (Menus) {
     // Add the gitHubAPI dropdown item
     Menus.addMenuItem('topbar', {
-      title: 'GitHubAPI',
+      title: 'GitHub API',
       state: 'gitHubAPI',
       type: 'dropdown',
       roles: ['*']
@@ -13,13 +13,13 @@ angular.module('gitHubAPI').run(['Menus',
 
     // Add the dropdown list item
     Menus.addSubMenuItem('topbar', 'gitHubAPI', {
-      title: 'List GitHubAPI',
+      title: 'List GitHub API',
       state: 'gitHubAPI.list'
     });
 
     // Add the dropdown call item
     Menus.addSubMenuItem('topbar', 'gitHubAPI', {
-      title: 'Call GitHubAPI',
+      title: 'Call GitHub API',
       state: 'gitHubAPI.call',
       roles: ['user']
     });
